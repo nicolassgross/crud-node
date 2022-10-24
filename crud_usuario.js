4b45a47e27a7a9be48ed33b43fb48742510aae1f
Nicolas <nicolas.gross@unimestre.com>
2022-10-24 08:34:48 -0300

    Adicionado método de alterar dados e remover usuários/Adicionando funções ao código

--- crud_usuario.js
+++ crud_usuario.js
@@ -17,10 +17,12 @@ console.clear();
 while (comeco == 1) {
     console.log(`
         Escolha uma das opções abaixo
-        1 - Cadastrar usuários
-        2 - Listar todos os usuários
-        3 - Listar usuários pelo nome
-        4 - Sair
+        1 - Cadastrar usuário
+        2 - Alterar usuário
+        3 - Remover usuário
+        4 - Listar todos os usuários
+        5 - Listar usuários pelo nome
+        6 - Sair
     `);
     var opcao_menu = prompt();
     console.clear();
@@ -49,46 +51,114 @@ while (comeco == 1) {
             idades.push(idade);
 
             console.clear();
-            console.log(`
-                Usuário cadastrado sucesso!
-
-                Deseja voltar ao comeco ou sair?
-                1 - Voltar ao comeco
-                2 - Sair
-            `);
-
-            if (prompt() == 2) {
-                comeco = 2;
-            }
-            console.clear();
         }
     } else if (opcao_menu == 2) {
+        //altera
+        var busca_id_alterar = prompt("Insira ID do usuário que deseja alterar o registro: ");
+
+        verificaArrayUsuariosId(busca_id_alterar);
+
+        console.log(`
+            Qual dado você deseja alterar?
+            1 - Nome
+            2 - Email
+            3 - Idade
+        `)
+
+        alterarDadosUsuario(busca_id_alterar);
+
+    } else if (opcao_menu == 3) {
+        //remove
+        var busca_id_remover = prompt("Insira ID do usuário que deseja remover o registro: ");
+
+        verificaArrayUsuariosId(busca_id_remover);
+
+        console.log(`
+            Tem certeza que deseja remover o dado acima?
+            1 - SIM
+            2 - NÃO
+        `)
+
+        removerDadosUsuario(busca_id_remover);
+
+    } else if (opcao_menu == 4) {
         for (var id = 0; id < contador; id++) {
             console.log(`
+                ID: ${id}
                 Nome: ${usuarios[id]}
                 E-mail: ${emails[id]}
                 Idade: ${idades[id]}
             `);
         }
-    } else if (opcao_menu == 3) {
+    } else if (opcao_menu == 5) {
         var busca_nome = prompt("Insira o nome ou sobrenome do usuário que deseja buscar(mínimo de 3 letras): ");
-        var achou = false;
 
-        for (var id = 0; id < contador; id++) {
-            if (busca_nome == usuarios[id].substring(0, 3)) {
-                console.log(`
-                    Nome: ${usuarios[id]}
-                    E-mail: ${emails[id]}
-                    Idade: ${idades[id]}
-                `);
-                achou = true;
-            }
-        }
-
-        if (achou == false) {
-            console.log('Nenhum usuário encontrado');
-        }
+        verificaArrayUsuariosNome(busca_nome);
     } else {
         comeco = 2;
+    }
+}
+
+function verificaArrayUsuariosNome(busca) {
+    var achou = false;
+
+    for (var id = 0; id < contador; id++) {
+        if (busca == usuarios[id].substring(0, 3)) {
+            console.log(`
+                ID: ${id}
+                Nome: ${usuarios[id]}
+                E-mail: ${emails[id]}
+                Idade: ${idades[id]}
+            `);
+            achou = true;
+        }
+    }
+    if (achou == false) {
+        console.log('Nenhum usuário encontrado');
+    }
+}
+
+function alterarDadosUsuario(busca) {
+    if (prompt() == 1) {
+        var nome = prompt("Insira o nome: ");
+
+        usuarios[nome].push(nome);
+    } else if (prompt() == 2) {
+        var email = prompt("Insira o email: ");
+       
+        emails.push(email);
+    } else if (prompt() == 3) {
+        var idade = prompt("Insira a idade: ");
+        
+        idades.push(idade);
+    }
+}
+
+function removerDadosUsuario(busca) {
+    if (prompt() == 1) {
+        usuarios.pop()
+        emails.pop()
+        idades.pop()
+    } else {
+
+    } 
+}
+
+function verificaArrayUsuariosId(busca) {
+    var achou = false;
+
+    for (var id = 0; id < contador; id++) {
+        if (busca == id) {
+            console.log(`
+                ID: ${id}
+                Nome: ${usuarios[id]}
+                E-mail: ${emails[id]}
+                Idade: ${idades[id]}
+            `);
+            achou = true;
+        }
+    }
+    if (achou == false) {
+        console.log('Nenhum usuário encontrado');
     }
 }
